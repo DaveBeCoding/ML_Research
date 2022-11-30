ec9353ac0354d3113c386d5eff5298b651a0b85e
davebecoding <david00100johnson@gmail.com>
2022-11-30 13:41:10 -0800

    Ax=B example linear system of equations for GMRES

--- ScientificProgrammer_/Assignment1.py
+++ ScientificProgrammer_/Assignment1.py
@@ -10,7 +10,9 @@ STOP = 0b11
 Create infrastructure for studying this algorithm
     Create a reasonable CLI (command-line interface) that allows the user to tweak settings like n, m and density (what % of elements are non-zero)
     Be able to generate random A matrices that will converge on a correct solution
-        I did this by randomly adding values between 0-1 for non-zero elements, except for along the diagonal which were all non-zero and set to cube root of n . With this approach, I was getting matrices that worked up to 1000x1000 which is as high as you'll need in the python phase of this course. Only the largest of my test matrices required restarts.
+        I did this by randomly adding values between 0-1 for non-zero elements, except for along the diagonal which were all non-zero and set to cube root 
+        of n . With this approach, I was getting matrices that worked up to 1000x1000 which is as high as you'll need in the python phase of this course. 
+        Only the largest of my test matrices required restarts.
     Be able to generate random dense f (100% non-zero, values 0-1).
     You should also have infrastructure for hardcoded test fixtures for testing etc
 Create classes and methods for mathematical concepts
@@ -18,7 +20,8 @@ Create classes and methods for mathematical concepts
         scalar add/multiply
         matrix/matrix add/multiply
         element indexing
-        normalize (assumes it's a vector (the second dimension is 1)). Whether you make Vector a special class or just use matrices with second dim=1 is up to you.
+        normalize (assumes it's a vector (the second dimension is 1)). Whether you make Vector a special class or just use matrices with second dim=1 is 
+        up to you.
         get euclidean norm (vector-only)
         dot product (vector-only)
         QR factorization
@@ -33,6 +36,7 @@ algorithm details: https://en.wikipedia.org/wiki/Generalized_minimal_residual_me
 class GMRES:
     def __init__(self) -> None:
         self.clear()
+        print('Create Matrix A')
         n = int(input("Enter Size of N rows "))
         m = int(input("Enter Size of M columns "))
         dens = int(input('Enter Density % of Matrix -> Example "25" for 25% '))
