b28648da7677ada812727b87775d9cc5dd2c33bb
davebecoding <david00100johnson@gmail.com>
2022-12-02 21:40:59 -0800

    updating branch with gmres algorithm, update with dynamic input from user -> proof of concept working

--- ScientificProgrammer_/Assignment1.py
+++ ScientificProgrammer_/Assignment1.py
@@ -78,8 +78,41 @@ class GMRES:
         # Start here once test methods are flowing correctly
         self.Creating_msg()
 
-    def gmres(self):
+    def gmres(self, A, b, x0, nmax_iter):
         print('Initializing Matrix')
+
+        r = b - np.asarray(np.dot(A, x0)).reshape(-1)
+        x = []
+        q = [0] * (nmax_iter)
+
+        x.append(r)
+
+        q[0] = r / np.linalg.norm(r)
+
+        h = np.zeros((nmax_iter + 1, nmax_iter))
+
+        # for k in range(nmax_iter):
+        #     y = np.asarray(np.dot(A, q[k])).reshape(-1)
+        for k in range(min(nmax_iter, A.shape[0])):
+            y = np.asarray(np.dot(A, q[k])).reshape(-1)
+
+            # for j in range(k):
+            for j in range(k + 1):
+                h[j, k] = np.dot(q[j], y)
+                y = y - h[j, k] * q[j]
+            h[k + 1, k] = np.linalg.norm(y)
+            if (h[k + 1, k] != 0 and k != nmax_iter - 1):
+                q[k + 1] = y / h[k + 1, k]
+
+            b = np.zeros(nmax_iter + 1)
+            b[0] = np.linalg.norm(r)
+
+            result = np.linalg.lstsq(h, b)[0]
+
+            x.append(np.dot(np.asarray(q).transpose(), result) + x0)
+
+        return x
+
 
     def menu(self):
         print('1. GMRES')
@@ -91,6 +124,16 @@ class GMRES:
 
         match choice:
             case 1:
+                A = np.matrix('1 1; 3 -4') #replace with dynamic matrix input 
+                b = np.array([3, 2])
+                x0 = np.array([1, 2])
+
+                e = 0
+                nmax_iter = 5
+
+                x = self.gmres(A, b, x0, nmax_iter)
+
+                print(x)
                 self.gmres()
             case 2:
                 self.build_matrix()
