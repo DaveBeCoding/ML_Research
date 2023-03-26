779b6bd2b7a55f325010cf8350970bcb6802c74d
Dave <david00100johnson@gmail.com>
2023-03-26 14:21:12 -0700

    Set up uBLAS serial solver with kitware build system, updating notes section(s)

--- tutorials/data-structures/cxx_ds/containers/adjacency_list/main.cxx
+++ tutorials/data-structures/cxx_ds/containers/adjacency_list/main.cxx
@@ -7,7 +7,6 @@ using namespace std;
 int main() {
     unordered_map<int, vector<int> > adjList;
 
-    // Add edges to the adjacency list
     adjList[0].push_back(1);
     adjList[0].push_back(2);
     adjList[1].push_back(2);
@@ -15,7 +14,6 @@ int main() {
     adjList[2].push_back(3);
     adjList[3].push_back(3);
 
-    // Print the adjacency list
     for (auto& pair : adjList) {
         cout << pair.first << ": ";
         for (int i : pair.second) {
