0096de2a99ee30a83369b95ab40efe1488c110a6
Dave <david00100johnson@gmail.com>
2023-03-27 22:23:53 -0700

    display histogram equation difference, add filesystem, and display image enhancements:ter

--- tutorials/data-structures/cxx_ds/cxx_libraries/opencvcxx/opencvcxx/main.cxx
+++ tutorials/data-structures/cxx_ds/cxx_libraries/opencvcxx/opencvcxx/main.cxx
@@ -1,15 +1,17 @@
 #include <opencv2/opencv.hpp>
-#include <iostream>
+#include <filesystem>
 
-using namespace std;
 using namespace cv;
+
+//namespace fs = std::filesystem;
+namespace fs = std::__fs::filesystem;
 
 int main() {
     // Load the image
     Mat img = imread("vskater.jpg", IMREAD_GRAYSCALE);
-    
+
     if (img.empty()) {
-        cerr << "Error: Could not read image file." << endl;
+        std::cerr << "Error: Could not read image file." << std::endl;
         return -1;
     }
 
@@ -17,11 +19,26 @@ int main() {
     Mat enhanced_img;
     equalizeHist(img, enhanced_img);
 
-    // Display the original and enhanced images
-    namedWindow("Original Image", WINDOW_AUTOSIZE);
-    imshow("Original Image", img);
-    namedWindow("Enhanced Image", WINDOW_AUTOSIZE);
-    imshow("Enhanced Image", enhanced_img);
+    // Compute the matrix value difference between the two images
+    Mat diff;
+    absdiff(img, enhanced_img, diff);
+
+    // Display the original and enhanced images side by side
+    Mat side_by_side(img.rows, img.cols * 2, img.type());
+    img.copyTo(side_by_side(Rect(0, 0, img.cols, img.rows)));
+    enhanced_img.copyTo(side_by_side(Rect(img.cols, 0, img.cols, img.rows)));
+    imshow("Original vs Enhanced", side_by_side);
+
+    // Save a copy of the "Original vs Enhanced" image in a directory named "output-image"
+    fs::path output_dir("../output-image");
+    if (!fs::exists(output_dir)) {
+        fs::create_directory(output_dir);
+    }
+    fs::path output_path = output_dir / "original_vs_enhanced.jpg";
+    imwrite(output_path.string(), side_by_side);
+
+    // Display the matrix value difference between the two images
+    imshow("Matrix Value Difference", diff);
 
     // Wait for a key press
     waitKey(0);
